739d7ab57ac91f9f173a940f5af2e37d9d0d9405
Mayank Panchal <indusmk90@gmail.com>
2026-01-13 18:47:45 +0530

    Add event listener for task input validation

--- Project/todo-app/script.js
+++ Project/todo-app/script.js
@@ -2,6 +2,7 @@ const btn = document.querySelector("button");
 const inp = document.querySelector("input");
 const task = document.querySelector(".tasklist");
 const del = document.querySelectorAll('.delete');
+
 btn.addEventListener("click", () => {
   if (inp.value.length === 0) {
     alert("enter your task");
